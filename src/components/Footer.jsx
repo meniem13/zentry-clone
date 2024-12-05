02f384cd8b1e282046d4a50ab8bc6ac72698e09e
meniem13 <meniemnasr84@gmail.com>
2024-12-05 04:26:23 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const socialLinks = [
 const Footer = () => {
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+      <div className="container mx-auto flex flex-col items-center justify-around gap-4 px-4  md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
           ©Nova 2024. All rights reserved
         </p>
